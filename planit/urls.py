92bec25727c8251d7f3634feb99c279ec95f32f9
James Pulec <jpulec@gmail.com>
2013-09-23 22:09:03 -0500

    On our way

---
+++ planit/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'$', Home.as_view(), name="home"),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Static-y pages
+    #url(r'^about/$', About.as_view(), name="about"),
+    #url(r'^contact/$', Contact.as_view(), name='contact'),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
